577dff1c02a0ec9dd41178d2cdf7544313e0f3a9
kaushal <kaushal.v.bhat@gmail.com>
2020-09-28 21:45:26 +0530

    add settings

--- server/tox_api/settings.py
+++ server/tox_api/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import json
+
+with open('./temp/config.json') as f:
+    config = json.load(f)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jskcxb4bk324bj23b423k44bj23b4j32432'
+SECRET_KEY = config['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
