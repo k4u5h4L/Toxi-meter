2e82b80515dd82643ae8f33f6f0005d546ebba78
kaushal <kaushal.v.bhat@gmail.com>
2020-09-28 21:43:10 +0530

    add setting

--- server/tox_api/settings.py
+++ server/tox_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yi*)@7#h(+l!1fwka(wn!a@$qkn#^21fi6q&e2jvmpz0ehpj$f'
+SECRET_KEY = 'jskcxb4bk324bj23b423k44bj23b4j32432'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
